a79469cf95d9e9c00ec7833489a06649275a5863
Levan Kiknavelidze <levank.1979@gmail.com>
2022-12-13 19:31:07 +0400

    4

--- src/main/java/ge/workshops/workshop1/Workshop1Application.java
+++ src/main/java/ge/workshops/workshop1/Workshop1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  Workshop1Application {
+public class Workshop1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Workshop1Application.class, args);
